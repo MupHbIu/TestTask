f29d5ad577abb5555e05eca602910cd9ea709250
MupHbIu <mack7698@gmail.com>
2021-02-13 15:33:56 +0300

    Appid

---
+++ app/src/main/java/com/test/testtask/util/ConstantsPrivate.kt
@@ -0,0 +1,8 @@
+package com.test.testtask.util
+
+object ConstantsPrivate {
+    const val BASE_URL_AUTHORIZATION = "https://api.openweathermap.org/"
+    const val APPID = "c35880b49ff95391b3a6d0edd0c722eb"
+    const val CITY_ID = 498817
+
+}
